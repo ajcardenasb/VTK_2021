ab10ed037a4b5aba2729cde0c93b7f8d94af594f
Anny Cárdenas <ajcardenasb1@gmail.com>
2021-01-25 10:17:56 +0100

    Create practiceR.R

---
+++ practiceR.R
@@ -0,0 +1,10 @@
+#1. set your working directory
+#2. load required libraries
+#3. import data and inspect data
+#4. filter dataframe to keep only individuals who have banana as their favorite fruit
+#5. filter dataframe to keep only individuals older than 50
+#6. calculate the average age of per favorite holiday
+#7. calculate the total number of pets per fruit
+#8. create a new column with the structure "ID - fruit - holiday"
+#9. replace mango by blueberry
+#10. create a new column with young/old (young= < 50)
